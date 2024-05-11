8832d0a34d41a9420bef959b0685191a340fb599
T4NG <farid.yusubov.1999@gmail.com>
2024-05-12 01:30:49 +0400

    Update reverse_dns_scan_t4ng.py
    Changes:
    
    Total number of ips in scope
    Removed % bar
    Added information about processing ip
    Added information about available domains count on each ip row
    Changed hostname file listing design

--- reverse_dns_scan_t4ng.py
+++ reverse_dns_scan_t4ng.py
@@ -1,11 +1,11 @@
 import subprocess
 import re
-from tqdm import tqdm
 from colorama import Fore, Style
+import ipaddress
 
 def print_banner():
+    print("\nMade by")
     print(r"""
-
 ████████╗░░██╗██╗███╗░░██╗░██████╗░
 ╚══██╔══╝░██╔╝██║████╗░██║██╔════╝░
 ░░░██║░░░██╔╝░██║██╔██╗██║██║░░██╗░
@@ -14,39 +14,50 @@ def print_banner():
 ░░░╚═╝░░░░░░░░╚═╝╚═╝░░╚══╝░╚═════╝░
                         """)
 
+def calculate_total_ips(ip_list):
+    total_ips = 0
+    for ip_range in ip_list:
+        if '/' in ip_range: 
+            total_ips += ipaddress.ip_network(ip_range, strict=False).num_addresses
+        else:
+            total_ips += 1
+    return total_ips
+
 def reverse_dns_scan(ip_list_file, output_filename):
+    total_domains = 0  
     with open(ip_list_file, 'r') as ip_file:
         ip_list = ip_file.read().splitlines()
     with open(output_filename, 'w') as output_file:
-        with tqdm(total=len(ip_list), desc="Scanning:", bar_format="{l_bar}%s{bar}%s{r_bar}" % (Fore.GREEN, Fore.RESET)) as pbar:
+        for ip_range in ip_list:
             count = 0
-            for ip_range in ip_list:
-                process = subprocess.Popen(['nmap', '-sL', ip_range], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                stdout, stderr = process.communicate()
-                if stderr:
-                    print(stderr.decode())
-                    continue
-                lines = stdout.decode().split('\n')
-                for line in lines:
-                    match = re.search(r'Nmap scan report for (.+) \((\d+\.\d+\.\d+\.\d+)\)', line)
-                    if match:
-                        hostname = match.group(1)
-                        ip = match.group(2)
-                        output_file.write(f"{count + 1}. {hostname} - {ip}\n")
-                        count += 1
-                pbar.update(1)
-                pbar.set_postfix_str(f"Scanned: {count}/{len(ip_list)}")
+            print(f"Scanning {ip_range}...")
+            process = subprocess.Popen(['nmap', '-sL', ip_range], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            stdout, stderr = process.communicate()
+            if stderr:
+                print(stderr)
+                continue
+            lines = stdout.split('\n')
+            for line in lines:
+                match = re.search(r'Nmap scan report for (.+) \((\d+\.\d+\.\d+\.\d+)\)', line)
+                if match:
+                    hostname = match.group(1)
+                    ip = match.group(2)
+                    output_file.write(f"{ip} - {hostname}\n")
+                    count += 1
+            total_domains += count 
+            print(f" DONE: {ip_range} | Available Domains: {count}\n")
 
-    return count 
+    return total_domains
 
 if __name__ == "__main__":
     ip_list_file = "ip_list.txt" 
     output_filename = "hostnames.txt"
     print_banner()
-    print("Script made by T4NG")
-    print("Reverse DNS Starting...")
-    print("System is running, don`t quit! The process can take 2-5 minute in ip/24 scope, other scopes takes more 5m+.")
-    num_hosts = reverse_dns_scan(ip_list_file, output_filename)
-    print(f"Scan done. {num_hosts} hosts added to 'hostnames.txt' file")
-    print("\nHave a nice day ;)")
-
+    
+    with open(ip_list_file, 'r') as ip_file:
+        ip_list = ip_file.read().splitlines()
+    total_ips = calculate_total_ips(ip_list)
+    print(f"\nTotal IPs in the list of scope: {total_ips}\n")
+    
+    total_domains = reverse_dns_scan(ip_list_file, output_filename)
+    print(f"\nScan done. Total {total_domains} domain found and saved to `{output_filename}` file.")
